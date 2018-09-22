c2185a9b670d34f9eeee6517323a5b6bb52ef85f
Andre Barbosa <an.barbosa@outlook.com.br>
2018-09-22 19:18:33 -0300

    Commit inicial. Estrutura inicial de cmake/make completa de forma que um exec chamado teste é criado na pasta build, contendo os arquivos em src e os headers em include.

---
+++ include/teste.h
@@ -0,0 +1,1 @@
+int TESTE = 1;
---
+++ src/test.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include "teste.h"
+
+using namespace std;
+
+int main(void) {
+
+     cout << "Hello World " << TESTE << endl;
+
+     return(0);
+
+}
